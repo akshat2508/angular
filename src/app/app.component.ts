07797d4907a6e7be6977c17bbd17fe6397397961
Akshat-paul <akshatpaul2006@gmail.com>
2025-09-09 20:51:54 +0530

    using signals to toggle and use settimeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,26 @@ import { isEqualsGreaterThanToken } from 'typescript';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-//   count = signal(10);
-// x =20;
+  userName = signal('akshat');
+  count  =signal(0);
 
-// constructor()
-// {
-//   effect(()=>{
-//     console.log(this.count());
-//   })
-// }
+  displayHeading = true;
+  constructor()
+  {
+    effect(()=>{
+      if(this.count( ) == 2)
+      {
+        setTimeout(() => {
+          this.displayHeading = false;
+        }, 2000);
+      }
+    })
+  }
 
-// updateValue()
-// {
-//   this.count.set(this.count()+1);
-// }
-  // data : WritableSignal<number>  = signal(10);
-  // // count: Signal<number> = computed(()=>200);
-  // updateSignal()
-  // {
-  //   // this.data.set("hello");
-  //   this.data.update((val)=> val + 1);
-  // }
-
-  // x = signal(10);
-  // y = signal(20);
-  // z= (()=>this.x() + this.y());
-  // updatedValue()
-  // {
-  //   console.log(this.z());
-  //   this.x.set(200);
-  //   console.log(this.z());
-  // }
-
+  toggleValue()
+  {
+   this.count.set(this.count() +1);
+  }
 
 
 
